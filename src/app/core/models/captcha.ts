617a40907bc89830054fefa0219f2b5bcb4cf2ec
sbeytour <beytour.safae@gmail.com>
2026-02-03 17:17:12 +0100

    implement the models

---
+++ src/app/core/models/captcha.ts
@@ -0,0 +1,32 @@
+export type CaptchaType = 'image' | 'text' | 'operation';
+
+export interface CaptchaStage {
+    id: number;
+    title: string;
+    type: CaptchaType;
+    instructions: string;
+    correctAnswers: any;
+    selectedImages?: any[];
+}
+
+export interface UserAnswer {
+    stageId: number;
+    answer: any;
+    isCorrect: boolean;
+    time: number;
+}
+
+export interface AppState {
+    currentSatgeId: number;
+    stages: CaptchaStage[];
+    userAnswers: UserAnswer[];
+    isStarted: boolean;
+    isCompleted: boolean;
+}
+
+export interface Result {
+  stageId: number;
+  title: string;
+  passed: boolean;
+  details: string;
+}
