95e14f154862c7bd8219f468ad489af4ab4b0acc
sbeytour <beytour.safae@gmail.com>
2026-02-03 17:19:33 +0100

    set up routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Captcha } from './components/captcha/captcha';
+import { Result } from './components/result/result';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: "home",
+        component: Home
+    },
+    {
+        path: "challenge",
+        component: Captcha
+    },
+    {
+        path: "result",
+        component: Result
+    }
+];
