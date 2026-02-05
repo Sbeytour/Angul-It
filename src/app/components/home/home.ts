3b9d4e6701b783acff73100986e81521767629bb
sbeytour <beytour.safae@gmail.com>
2026-02-05 08:27:39 +0100

    home page

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButton, MatFabButton } from '@angular/material/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatFabButton
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+  router = inject(Router)
 
+  startChallenge() {
+    this.router.navigate(['/challenges'])
+  }
 }
